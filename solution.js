bc62ebee13a8164f3cd2171a0ac52d77d20b50d5
S Rabi <rabi.sanaye@yahoo.com>
2022-11-29 15:27:27 +0100

    ex1 done

--- solution.js
+++ solution.js
@@ -1,3 +1,11 @@
+for (let i = 3; i < process.argv.length; i++) {
+  if (isNaN(process.argv[i])) {
+    console.log(
+      `Sorry, the argument '${process.argv[i]}' is not a number, please try again`
+    );
+  }
+}
+
 if (process.argv[2] === "sum") {
   // 1. calculating the sum
   console.log(
@@ -14,5 +22,8 @@ if (process.argv[2] === "sum") {
       process.argv.slice(3).length
     ).toString()
   );
+} else {
+  console.log(
+    'I cannot calculate that, please type either "sum" (to calculate the sum) or "avg" (To calculate the Average)'
+  );
 }
-// else if
